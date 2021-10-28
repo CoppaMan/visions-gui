37739db57f1157b551ac333d5596e55b216deff6
dario <dario.mp@bluewin.ch>
2021-10-28 16:03:25 +0200

    Create image folder if it doesn't exist

--- visions-gui.py
+++ visions-gui.py
@@ -1,6 +1,11 @@
+import os
 import logging
 from gui.gui_elements import VisionGUI
 
 logging.basicConfig(level=logging.DEBUG)
+
+if not os.path.exists('images'):
+    os.makedirs('images')
+
 root = VisionGUI()
 root.show()
